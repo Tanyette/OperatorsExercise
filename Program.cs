50ed45baa2588ba11a6f5845c3bef840f736de07
Tanyette <97054658+Tanyette@users.noreply.github.com>
2022-02-27 03:51:04 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var a = 17;
+            var b = 4;
+
+            //Addition
+            var sum = a + b;
+            Console.WriteLine($"Addition: {sum}");
+
+            //Subtraction
+            var diff = a - b;
+            Console.WriteLine($"Subtraction: {diff}");
+
+            //Multiplication
+            var product = a * b;
+            Console.WriteLine($"Multiplication: {product}");
+
+            //Division
+            var quotient = a / b;
+            
+            var remainder = a % b;
+            Console.WriteLine($"The quotient is {quotient}with a remainder of {remainder}");
+            Console.WriteLine();
+            */
+
+            Console.WriteLine("what is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            var circle = AreaOfCircle(radius);
+            Console.WriteLine($"The area of your circle is {circle}");
+
+        }
+
+        public static double AreaOfCircle(double radius) 
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        } 
+    }
+}      
